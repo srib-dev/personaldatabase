6ce4423e97e83cdf1425399c8d90cea354b85187
Sondre Garnæs <sondre433@gmail.com>
2026-02-12 14:51:49 +0100

    Initial commit

---
+++ src/main/java/com/srib/personaldatabase/PersonaldatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.srib.personaldatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonaldatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonaldatabaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/srib/personaldatabase/PersonaldatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srib.personaldatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonaldatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
