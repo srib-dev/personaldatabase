99eaecd606ef76375a7db069e39f5211738214c0
jono <jon.noe.hoye@gmail.com>
2026-02-19 14:54:09 +0100

    sortere etter roller

--- src/main/java/com/srib/personaldatabase/PersonController.java
+++ src/main/java/com/srib/personaldatabase/PersonController.java
@@ -13,20 +13,21 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "Person API", description = "API for å håndtere personer")
 
 public class PersonController {
-    private final PersonRepository personRepository;
 
-    @Operation(summary = "Hent alle personer", description = "Returnerer en liste med alle personer")
-    @GetMapping
-    public List<Person> getAllPersons() {
-        return personRepository.findAll();
-    }
+    private final PersonRepository personRepository;
     @Operation(summary = "Legge til en person", description = "Legger til en person")
     @PostMapping
     public Person createPerson(@RequestBody Person person) {
         return personRepository.save(person);
     }
 
-
-
+    @Operation(summary = "Hent personer", description = "Henter alle eller filtrerer på rolle")
+    @GetMapping
+    public List<Person> getPersons(@RequestParam(required = false) String role) {
+    if (role != null && !role.isEmpty()) {
+        return personRepository.findByRole(role);
+    }
+    return personRepository.findAll();
+    }
 
 }
--- src/main/java/com/srib/personaldatabase/PersonRepository.java
+++ src/main/java/com/srib/personaldatabase/PersonRepository.java
@@ -3,7 +3,9 @@ package com.srib.personaldatabase;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
-
+    List<Person> findByRole(String role);
 }
