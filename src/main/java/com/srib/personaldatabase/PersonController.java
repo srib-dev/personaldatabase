8f95a7daf7b3c8b1ed07393cd7d8d356e86ee5c7
jono <jon.noe.hoye@gmail.com>
2026-02-19 15:32:04 +0100

    Nå kan man kalle med /role/{rolle}

--- src/main/java/com/srib/personaldatabase/PersonController.java
+++ src/main/java/com/srib/personaldatabase/PersonController.java
@@ -1,11 +1,17 @@
 package com.srib.personaldatabase;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/persons")
@@ -13,21 +19,23 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "Person API", description = "API for å håndtere personer")
 
 public class PersonController {
-
     private final PersonRepository personRepository;
+
+    @Operation(summary = "Hent alle personer", description = "Returnerer en liste med alle personer")
+    @GetMapping
+    public List<Person> getAllPersons() {
+        return personRepository.findAll();
+    }
     @Operation(summary = "Legge til en person", description = "Legger til en person")
     @PostMapping
     public Person createPerson(@RequestBody Person person) {
         return personRepository.save(person);
     }
 
-    @Operation(summary = "Hent personer", description = "Henter alle eller filtrerer på rolle")
-    @GetMapping
-    public List<Person> getPersons(@RequestParam(required = false) String role) {
-    if (role != null && !role.isEmpty()) {
-        return personRepository.findByRole(role);
-    }
-    return personRepository.findAll();
+    @Operation(summary = "Hent person utifra rolle", description = "Returnerer en liste med alle personer i rollen")
+    @GetMapping("/role/{roleName}")
+    public List<Person> getPersonsByRole(@PathVariable String roleName) {
+        return personRepository.findByRole(roleName);
     }
 
 }
