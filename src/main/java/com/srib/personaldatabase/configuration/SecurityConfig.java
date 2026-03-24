f77abea86ba19b8a86dea8fca6524b1bad0ad1a1
pixel_viking <carleirikdj@gmail.com>
2026-03-24 05:34:34 +0100

    fix: added tmp SecurityConfig

---
+++ src/main/java/com/srib/personaldatabase/configuration/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.srib.personaldatabase.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+// TODO: Replace with propper security config
+// This is temporary configuration that leaves it open to anybody to access the application
+// ONLY FOR DEVELOPMENT PURPOSES
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll())
+            .build();
+    }
+}
